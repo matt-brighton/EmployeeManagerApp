f3257b25c45b92f6b0fc3f5fc19f836a7237baac
matt-brighton <mat.brighton@hmrc.gov.uk>
2023-08-29 11:49:58 +0100

    Adding Testing

--- EmployeeManagerTests/Tests.cs
+++ EmployeeManagerTests/Tests.cs
@@ -8,17 +8,23 @@ namespace EmployeeManagerTests
         [Test, Order(1)]
         public void TestSaveEmployee()
         {
+            // Create an instance of the EmployeeRepository class to interact with employee data.
             EmployeeRepository repository = new EmployeeRepository();
+            // Create a new Employee object with the specified details.
             Employee employee = new Employee
             {
                 FirstName = "John",
                 LastName = "Doe",
                 Department = "IT"
             };
+            
+            // Save the newly created employee using the repository.
             repository.SaveEmployee(employee);
 
+            // Search for an employee with the given first name "John" and last name "Doe".
             employee = repository.SearchEmployee("John", "Doe");
 
+            // Use the Assert.NotNull method to verify that the employee object is not null.
             Assert.NotNull(employee);
 
         }
@@ -26,23 +32,35 @@ namespace EmployeeManagerTests
         [Test, Order(2)]
         public void TestSearchEmployee()
         {
+            // Create an instance of the EmployeeRepository class to interact with employee data.
             EmployeeRepository repository = new EmployeeRepository();
+
+            // Search for an employee with the given first name "John" and last name "Doe".
             Employee employee = repository.SearchEmployee("John", "Doe");
 
+            // Use the Assert.NotNull method to verify that the employee object is not null.
             Assert.NotNull(employee);
+
+            // Use the Assert.AreEqual method to verify that the employee's first name is "John".
             Assert.AreEqual("John", employee.FirstName);
+
+            // Use the Assert.AreEqual method to verify that the employee's last name is "Doe".
             Assert.AreEqual("Doe", employee.LastName);
         }
 
         [Test, Order(3)]
         public void TestDeleteEmployee()
         {
+            // Create an instance of the EmployeeRepository class to interact with employee data.
             EmployeeRepository repository = new EmployeeRepository();
 
+            // Delete employee with the given first name "John" and last name "Doe".
             repository.DeleteEmployee("John", "Doe");
 
+            // Search for an employee with the given first name "John" and last name "Doe".
             Employee employee = repository.SearchEmployee("John", "Doe");
 
+            // Use the Assert.NotNull method to verify that the employee object is null.
             Assert.IsNull(employee);
 
         }
